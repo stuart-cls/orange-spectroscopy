5fc3dbab32451617f17b9ce9fa206c8311f49b41
Stuart Read <stuart.read@lightsource.ca>
2024-07-19 11:50:36 -0600

    io.sgmdata: Read files with multiple NXdata entries

--- orangecontrib/spectroscopy/io/cls.py
+++ orangecontrib/spectroscopy/io/cls.py
@@ -40,39 +40,52 @@ class HDF5Reader_SGM(FileFormat, SpectralFileFormat):
                 for i, d in
                 enumerate(NXdata)]
 
-            # for i, d in enumerate(NXdata):
-            # starting with single entry (0)
-            i = 0
-            d = NXdata[0]
+            features_entries = []
+            X_entries = []
+            meta_table_entries = []
+            for i, d in enumerate(NXdata):
+                d = NXdata[i]
 
-            if len(axes[i]) == 1:
-                warnings.warn(f"1D datasets not yet implemented: {d} not loaded.")
-            x_locs = h5[d][axes[i][0]]
-            y_locs = h5[d][axes[i][1]]
-            en = h5[d]['en']
-            emission = h5[d]['emission']
+                if len(axes[i]) == 1:
+                    warnings.warn(f"1D datasets not yet implemented: {d} not loaded.")
+                x_locs = h5[d][axes[i][0]]
+                y_locs = h5[d][axes[i][1]]
+                en = h5[d]['en']
+                emission = h5[d]['emission']
 
-            X_data = {}
-            meta_table = None
-            meta_data = {}
-            for k, v in data[i].items():
-                dims = len(v.shape)
-                _, spectra, meta_table = _spectra_from_image(np.transpose(np.atleast_3d(v), (1, 0, 2)),
-                                                             None, x_locs, y_locs)
-                if dims == len(axes[i]) + 1 and self.sheet in [k, "All"]:
-                    # sdd-type 3D data
-                    X_data[k] = spectra
-                elif dims == len(axes[i]):
-                    # mapping scalars
-                    meta_data[k] = spectra
-            X = np.concatenate(list(X_data.values()), axis=-1)
+                X_data = {}
+                meta_table = None
+                meta_data = {}
+                for k, v in data[i].items():
+                    dims = len(v.shape)
+                    _, spectra, meta_table = _spectra_from_image(np.transpose(np.atleast_3d(v), (1, 0, 2)),
+                                                                 None, x_locs, y_locs)
+                    if dims == len(axes[i]) + 1 and self.sheet in [k, "All"]:
+                        # sdd-type 3D data
+                        X_data[k] = spectra
+                    elif dims == len(axes[i]):
+                        # mapping scalars
+                        meta_data[k] = spectra
+                X = np.concatenate(list(X_data.values()), axis=-1)
+                X_entries.append(X)
 
-            features = np.arange(X.shape[-1])
-            if len(features) == len(emission):
-                features = np.array(emission)
+                features = np.arange(X.shape[-1])
+                if len(features) == len(emission):
+                    features = np.array(emission)
+                features_entries.append(features)
 
-            meta_table = meta_table.add_column(ContinuousVariable("en"), np.ones(X.shape[0]) * en, to_metas=True)
-            for k, v in meta_data.items():
-                meta_table = meta_table.add_column(ContinuousVariable(k), v[:, 0], to_metas=True)
+                meta_table = meta_table.add_column(ContinuousVariable("en"), np.ones(X.shape[0]) * en, to_metas=True)
+                for k, v in meta_data.items():
+                    meta_table = meta_table.add_column(ContinuousVariable(k), v[:, 0], to_metas=True)
+                meta_table_entries.append(meta_table)
 
-            return features, X, meta_table
+            if not all(np.array_equal(f, features_entries[0]) for f in features_entries):
+                warnings.warn("Multiple NXdata entries with incompatible shape.")
+            try:
+                X = np.vstack(X_entries)
+            except:
+                warnings.warn("Multiple NXdata entries with incompatible shape.\nLoading first entry only.")
+                return features_entries[0], X_entries[0], meta_table_entries[0]
+            meta_table = meta_table.concatenate(meta_table_entries)
+
+            return features_entries[0], X, meta_table
